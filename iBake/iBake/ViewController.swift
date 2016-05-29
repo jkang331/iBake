06fe5a550931e4f7488c72cb6b066ef3e0dd76cc
Jennifer Kang <jkang331@uw.edu>
2016-05-28 18:17:52 -0700

    added initial table view with table cell

--- iBake/iBake/ViewController.swift
+++ iBake/iBake/ViewController.swift
@@ -7,12 +7,53 @@
 //
 
 import UIKit
+import Foundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var RecipesList: UITableView!
+    
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        //TODO: switch to Recipe View
+        //let secondViewController = self.storyboard!.instantiateViewControllerWithIdentifier("question") as! QuestionController
+        //secondViewController.subject = retrieveSubjectsList()[indexPath.row]
+        //secondViewController.questionsList = questionsList[secondViewController.subject!]
+        //self.presentViewController(secondViewController, animated: true, completion: nil)
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5; //TODO: this will be dependend on the number of recipes we have
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 75.0;
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let recipeIdentifier = "recipe"
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier(recipeIdentifier) as! RecipeCell
+        
+        //Configuring the cell
+        cell.Title.text = "Recipe Name"
+        cell.PrepTime.text = "20 min"
+        cell.RecipeImage?.image = UIImage(named: "pusheen_letsbake.jpg" )
+        
+        
+        
+        //Adding a Separator Line to the Bottom
+        let separatorLineView = UIView.init(frame: CGRectMake(0, cell.frame.size.height - 0.5 , self.view.frame.width, 1))
+        separatorLineView.backgroundColor = UIColor.lightGrayColor()
+        cell.addSubview(separatorLineView)
+        
+        return cell;
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.RecipesList.delegate = self
+        self.RecipesList.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
