36b0a9d53afbf8626c021651330bc04efcd93db0
iGuest <jkang331@uw.edu>
2016-05-31 20:01:50 -0700

    handled extra long recipe names, working on parsing ingredients

--- iBake/iBake/StepController.swift
+++ iBake/iBake/StepController.swift
@@ -9,7 +9,19 @@
 import UIKit
 
 class StepController: UIViewController{
+    var recipeDictionary : NSDictionary?
+    var recipeName : String?
+    
+    @IBOutlet weak var previousButton: UIBarButtonItem!
+    @IBOutlet weak var nextButton: UIBarButtonItem!
+    
     @IBOutlet weak var Navbar: UINavigationBar!
+    @IBOutlet weak var stepTitle: UILabel!
+    @IBOutlet weak var stepLabel: UILabel!
+    
+    var counter = 1;
+    var displayedIngredients = false;
+    
     
     @IBAction func previousStepSelected(sender: AnyObject) {
     }
@@ -24,8 +36,45 @@ class StepController: UIViewController{
     }
     
     
+    func updateTitle() {
+        if (!displayedIngredients){
+            stepTitle.text = "Ingredients"
+            previousButton.enabled = false
+        } else {
+            stepTitle.text = "Step \(counter)"
+            counter += 1
+        }
+    }
+    
+    func updateLabel() {
+        if (!displayedIngredients){
+            // parse ingredients
+            //not sure what this struct is
+            let ingredientsBlock = recipeDictionary!["Ingredients"]
+//            print(ingredientsBlock.count)
+            print(recipeDictionary!["Ingredients"])
+            print(recipeDictionary!["Ingredients"])  //get's main ingredient
+        } else {
+            //get from instructions
+            stepLabel.text = String(recipeDictionary!["Instructions"])
+            print(recipeDictionary!["Instructions"])
+        }
+        
+        
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        Navbar.topItem!.title = recipeName
+        updateTitle()
+        updateLabel()
+        
+        
+        print(recipeDictionary!["TotalMinutes"])
+        print(recipeDictionary!["PrimaryIngredient"])
         
     }
     
